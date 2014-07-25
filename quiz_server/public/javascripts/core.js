c89e72e4c8776def63d816ee6e418b74bca007d3
mauvemoonman <mauvemoonman@gmail.com>
2014-07-25 21:53:22 +0000

    finished edit... mostly

--- quiz_server/public/javascripts/core.js
+++ quiz_server/public/javascripts/core.js
@@ -2,52 +2,237 @@
 
 //Make Hops Website
 
-var pocketQuest = angular.module('pocketQuest', []);
+var quizProject = angular.module('quizProject', []);
 
-function mainController($scope, $http) {
-	$scope.formData = {};
-	$scope.quiz_name = {};
-	$scope.quiz_genre = "";
-	$scope.quiz_questions = "";
-
-	// when landing on the page, get all todos and show them
-	$http.get('/all')
-		.success(function(data) {
-			console.log(data[0]);
-			$scope.quizzes = data[0].tests;
-		})
-		.error(function(data) {
-			console.log('Error: ' + data);
-		});
+function mainController($scope, $http, $compile) {
+	var quiz_name = "";
 	
-	$scope.quiz_name.nameClick = function(name,genre,questions,event){
+	//A holder for the post form data
+	$scope.formData = {};
+	//All the quizzes
+	$scope.quizzes = [];
+	//The currently selected quiz
+	$scope.quiz_name = "";
+	//The currently selected quiz
+	$scope.quiz = {};
+	//The edit_questions that will be added...
+	$scope.editing_answers = [{id: 'choice1'}];
+
+	 $scope.loadData = function () {
+		 console.log("Loading Page");
+	     $http.get('/all').success(function(data) {
+	    	 //console.log("Load returned success!");
+	    	 $scope.quizzes = data;
+	    	 for(quiz in $scope.quizzes){
+	    		 var this_quiz = $scope.quizzes[quiz];
+	    		 //console.log("Comparing: " + $scope.quiz['name'] + ":" + this_quiz['name']);
+	    		 if($scope.quiz['name'] == this_quiz['name']){
+	    			 //console.log("Found Match!!!");
+	    			 $scope.quiz = this_quiz;
+	    		     $scope.nameClick($scope.quiz,false);
+	    		 }
+	    	 }
+	     });
+	  };
+	
+	$scope.tabClick = function(tab_name){
+		console.log(tab_name);
+		if(tab_name == "home"){
+			$('#home_nav').addClass("active");
+			$('#edit_nav').removeClass("active");
+			$('#view_nav').removeClass("active");
+			
+			$('#home_tab').show();
+			$('#edit_tab').hide();
+			$('#view_tab').hide();
+		}else if(tab_name == "edit"){
+			$('#home_nav').removeClass("active");
+			$('#edit_nav').addClass("active");
+			$('#view_nav').removeClass("active");
+			
+			$('#home_tab').hide();
+			$('#edit_tab').show();
+			$('#view_tab').hide();
+		}else{
+			$('#home_nav').removeClass("active");
+			$('#edit_nav').removeClass("active");
+			$('#view_nav').addClass("active");
+			
+			$('#edit_tab').hide();
+			$('#home_tab').hide();
+			$('#view_tab').show();
+		}
+		$('.meta_child').hide();
+	};
+	
+	$scope.nameClick = function(quiz, clicked/*,genre,questions,event*/){
 		console.log("Clicked Something");
-		$scope.quiz_name = name;
-		console.log($scope.quiz_name);
-		$scope.quiz_genre = genre;
-		console.log($scope.quiz_genre);
-		$scope.quiz_questions = questions;
-		console.log($scope.quiz_questions);
-		$('.meta_child').show();
+		if($scope.quiz == quiz && clicked){
+			$scope.quiz = {};
+			$('.meta_child').hide();
+		}else{
+			$scope.quiz = quiz;
+			$scope.quiz_name = $scope.quiz['name'];
+			$scope.quiz_questions = quiz['questions'];
+			var question_string = "";
+			for(var i = 0; i < quiz['questions'].length; i++){
+				question_string += question_formatter(quiz['questions'][i]);
+			}
+			question_string += add_question();
+			console.log(question_string);
+			$('.question_holder').html($compile($(question_string).contents())($scope));
+			$('.meta_child').show();
+		}
 	};
 
-	$scope.convertMeta = function(meta){
+	/*$scope.convertMeta = function(meta){
 		console.log(meta);
 		var json = JSON.stringify(eval("(" + meta + ")"));
 		console.log(json);
+	};*/
+	
+	$scope.addNewChoice = function() {
+		var newItemNo = $scope.editing_answers.length+1;
+		$scope.editing_answers.push({'id':'choice'+newItemNo});
+	};
+	
+	//This makes sure that the add button only appears next to the last answer in the form
+	$scope.showAddChoice = function(choice) {
+		return choice.id === $scope.editing_answers[$scope.editing_answers.length-1].id;
+	};
+	
+	$scope.add_test = function(){
+		if($('.add_quiz').is(":visible") ){
+			$('.add_quiz').hide();
+			$('.add_quiz_holder a').addClass('glyphicon-plus');
+			$('.add_quiz_holder a').removeClass('glyphicon-remove');
+			reload_form_data();
+		}else{
+			$('.add_quiz').show();
+			$('.add_quiz_holder a').removeClass('glyphicon-plus');
+			$('.add_quiz_holder a').addClass('glyphicon-remove');
+		}
+	};
+	
+	$scope.saveQuiz = function(test){
+		console.log("Delete Clicked!");
+		$scope.formData = {"genre":test['genre'],"test":test['name']};
+		console.log($scope.formData);
+		$http.post('/add_quiz', $scope.formData)
+			.success(function(data) {
+				console.log("Add Quiz Returned Success!!!");
+				reload_form_data();
+				$scope.loadData();
+			})
+			.error(function(data) {
+				console.log('Error: ' + data);
+			});
+	}
+	
+	$scope.add_question = function(){
+		if($('.add_form').is(":visible") ){
+			$('.add_form').hide();
+			$('.add_div a').addClass('glyphicon-plus');
+			$('.add_div a').removeClass('glyphicon-remove');
+			reload_form_data();
+		}else{
+			$('.add_form').show();
+			$('.add_div a').removeClass('glyphicon-plus');
+			$('.add_div a').addClass('glyphicon-remove');
+		}
 	};
 
-	// when submitting the add form, send the text to the node API
-	$scope.createUser = function() {
-		$http.post('/add_user', $scope.formData)
+	$scope.saveQuestion = function(question){
+		console.log("New Question Saved!!!");
+		console.log(question);
+		var answers = [];
+		for(answer in $scope.editing_answers){
+			answers.push($scope.editing_answers[answer].name);
+		}
+		question['answers'] = answers;
+		question['test'] = $scope.quiz['name'];
+		console.log(question);
+
+		$scope.formData = question;
+		console.log($scope.formData);
+		
+		$http.post('/add_question', $scope.formData)
 			.success(function(data) {
-				$scope.formData = {}; // clear the form so our user is ready to enter another
-				$scope.todos = data;
-				console.log(data);
+				console.log("Add returned success!!!");
+				reload_form_data();
+				$scope.loadData();
+			})
+			.error(function(data) {
+				console.log('Error: ' + data);
+			});
+		$scope.nameClick($scope.quiz,false);
+	};
+	
+	$scope.delete_question = function(question){
+		console.log("Delete Clicked!");
+		$scope.formData = {"question":question,"test":$scope.quiz['name']};
+		console.log($scope.formData);
+		$http.post('/delete_question', $scope.formData)
+			.success(function(data) {
+				console.log("Delete Returned Success!!!");
+				$scope.loadData();
 			})
 			.error(function(data) {
 				console.log('Error: ' + data);
 			});
 	};
+	
+	function reload_form_data(){
+		$scope.question = {};
+		$scope.new_quiz = {};
+		$scope.editing_answers = [{id: 'choice1'}];
+		$('.add_form').hide();
+		$('.add_div a').addClass('glyphicon-plus');
+		$('.add_div a').removeClass('glyphicon-remove');
+		$('.add_quiz').hide();
+		$('.add_quiz_holder a').addClass('glyphicon-plus');
+		$('.add_quiz_holder a').removeClass('glyphicon-remove');
+	}
+	
+	function question_formatter(question){
+		var answer_string = "";
+		for(var i = 0; i < question["answers"].length; i++){
+			answer_string += "<p>" + (i+1) + ") " + question["answers"][i] + "</p>";
+		}
+		return "<div><div class='name_div well'><a class='remove glyphicon glyphicon-remove' href='' ng-click='delete_question(\""+question["question"]+"\")'></a>" +
+				"<p class='meta_data label label-info'><b>Question:</b></p><p><p>" + question["question"] + "</p></p>" +
+				"<p class='meta_data label label-info'><b>Answers:</b></p><p>" + answer_string + "</p>" +
+				"<p class='meta_data label label-info'><b>Correct:</b></p><p><p>" + question["correct"] + "</p></p>" +
+				"</div></div>";
+	}
+	
+	function add_question(){
+		return "<div><div class='add_div well'>" +
+				"<a class='remove glyphicon glyphicon-plus' href='' ng-click='add_question()'></a>" +
+				"<div class=add_form style='display: none'><form>" +
+				"<p class='meta_data label label-info'><b>Question:</b></p>" +
+				"<p><p><input type='text' ng-model='question.question' placeholder='question'/></p></p>" +
+				"<p class='meta_data label label-info'><b>Answer:</b></p>" +
+				"<div class='form-group' data-ng-repeat='answer in editing_answers'>"+
+				  "<label for='answer' ng-show='showChoiceLabel(answer)'>Answer</label>" + 
+				  "<input type='text' ng-model='answer.name' name='' placeholder='answer'>" +
+				  "<button ng-show='showAddChoice(answer)' ng-click='addNewChoice()'>Add another answer</button>" +
+				"</div>" + 
+				"<p class='meta_data label label-info'><b>Correct:</b></p><p><p><input type='number' ng-model='question.correct' placeholder='correct answer'/></p></p>" +
+				"<input type='submit' value='Save' ng-click='saveQuestion(question)' />" + 
+				"</form></div>" +
+				"</div></div>";
+	}
+	
+	/*function isEmpty(obj) {
+	    for(var prop in obj) {
+	        if(obj.hasOwnProperty(prop))
+	            return false;
+	    }
 
+	    return true;
+	}*/
+	
+	$scope.loadData();
+	
 }
--- quiz_server/routes/index.js
+++ quiz_server/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var http = require('http');
 
 
-var new_test_meta =  /*{tests:*/[{name:"dmv_test",genre:"education",questions:[{
+var new_test_meta =  /*{tests:*/{name:"dmv_test",genre:"education",questions:[{
   	question:"A pre-trip inspection should be completed",
   	answers:["at least once a week","if any problems occurred the last the time the vehicle was operated","before operating the vehicle"],
   	correct:3},
@@ -12,7 +12,7 @@ var new_test_meta =  /*{tests:*/[{name:"dmv_test",genre:"education",questions:[{
   	answers:["Make sure you are driving slow enough so you can stop within the range of your headlights in an emergency",
   	"Roll down your window so that the fresh air will help keep you awake","If you are sleepy, drink coffee or other caffeine products"],
   	correct:1}]
-  	}]
+  	}
   /*}*/;
 
 
@@ -26,42 +26,124 @@ router.get('/quiz', function(req, res) {
     res.sendfile(req.abs_path + '/views/user_index.html');
 });
 
-/*GET saved meta*/
+/*GET all meta*/
 router.get('/all', function(req, res) {
-
-    // Set our collection
     var db = req.db;
     var collection = db.get('quiz');
-
     collection.find({},{},function(e,docs){
         res.setHeader('Content-Type', 'application/json');
-        //var meta = JSON.stringify(docs[0].meta).replace(/"/g, '').replace("\\", '');
         res.send(docs);
     });
 });
 
-router.get('/reset', function(req, res) {
-	// Set our internal DB variable
+router.post('/add_quiz', function(req, res) {
     var db = req.db;
-    // Set our collection
-    var collection = db.get('quiz');
 
-    collection.remove({}, function(err) { 
-    	  console.log('collection removed');
-    });
+	console.log(req.param('test'));
+	console.log(req.param('genre'));
+
+    var test_name = req.param('test');
+    var test_genre = req.param('genre');
     
-    // Submit to the DB
-    collection.insert({"tests" : new_test_meta},
+    var collection = db.get('quiz');
+    collection.insert({name:test_name,genre:test_genre,questions:[]},
     	function (err, doc) {
 	        if (err) {
-	            // If it failed, return error
 	            res.send("Error");
 	        }
 	        else {
-	            // If it worked, send back success to the client
 	            res.send("Success");
 	        }
-    }
+    	}
+    );
+});
+
+router.post('/add_question', function(req, res) {
+      var db = req.db;
+
+	  console.log(req.param('test'));
+	  console.log(req.param('question'));
+
+      var test_name = req.param('test');
+      var question_correct = req.param('correct');
+      var question_name = req.param('question');
+      var question_answers = req.param('answers');
+      
+      var question_object = {
+		  question:question_name,
+		  answers:question_answers,
+		  correct:question_correct
+      }
+      
+      console.log(question_object);
+
+      // Set our collection
+      var collection = db.get('quiz');
+      
+      collection.find({name:test_name},{},function(e,docs){
+          	res.setHeader('Content-Type', 'application/json');
+          	console.log(docs[0].questions);
+          	docs[0].questions.push(question_object);
+          	collection.update({name:test_name}, {'$set':{"questions" : docs[0].questions}},function (err, doc) {
+                if (err) {
+                    res.send("Error");
+                }
+                else {
+                	res.send("Success");
+                }
+          	});
+      });
+});
+
+router.post('/delete_question', function(req, res) {
+      var db = req.db;
+
+	  console.log(req.param('test'));
+	  console.log(req.param('question'));
+
+      var test_name = req.param('test');
+      var question_name = req.param('question');
+
+      // Set our collection
+      var collection = db.get('quiz');
+      
+      collection.find({name:test_name},{},function(e,docs){
+          	res.setHeader('Content-Type', 'application/json');
+          	var new_questions = [];
+          	console.log(docs[0]);
+          	console.log(docs[0].questions);
+          	for(var i = 0; i < docs[0].questions.length;i++){
+          		console.log(docs[0].questions[i].question);
+          		if(docs[0].questions[i].question != question_name){
+          			new_questions.push(docs[0].questions[i]);
+          		}
+          	}
+          	collection.update({name:test_name}, {'$set':{"questions" : new_questions}},function (err, doc) {
+                if (err) {
+                    res.send("Error");
+                }
+                else {
+                	res.send("Success");
+                }
+          	});
+      });
+});
+
+router.get('/reset', function(req, res) {
+    var db = req.db;
+    var collection = db.get('quiz');
+    collection.remove({}, function(err) { 
+    	  console.log('collection removed');
+    });
+    collection.insert(new_test_meta,
+    	function (err, doc) {
+	        if (err) {
+	            res.send("Error");
+	        }
+	        else {
+	            res.send("Success");
+	        }
+    	}
     );
 });
 
